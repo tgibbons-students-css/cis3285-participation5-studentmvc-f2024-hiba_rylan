d87540fec61237f912886c3dbe8c732b93da880e
Bamlak Amedie <bamedie@css.edu>
2024-10-03 14:05:31 -0500

    Hiba and Bamlak are done with the learning activity.

---
+++ CIS3285_Unit4_StudentMVC_2024/Models/NullStudent.cs
@@ -0,0 +1,29 @@
+﻿using System.Xml.Linq;
+
+namespace CIS3285_Unit4_StudentMVC_2024.Models
+{
+    public class NullStudent : IStudentInterface
+    {
+        public NullStudent()
+        {  }
+        public int Id
+        {
+            get { return -1; }   
+            set {  }
+        }
+
+        public string Name
+        {
+            get { return "Null Student"; }   
+            set {  }
+        }
+
+        public int Credits
+        {
+            get { return -999; }   
+            set { }
+        }
+
+
+    }
+}
